3d1a3d81135b273b7e1af1257eb67f32b668c34d
shreelaxmikamath <shreelaxmi.k111@gmail.com>
2024-05-30 20:15:06 +0530

    check

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>AMC | Welcome hello</title>
+    <title>AMC | Welcome </title>
     <!-- <link rel="stylesheet" type="text/css" href="css/font.css"> -->
     <link rel="stylesheet" type="text/css" href="css/reg.css">
     <?php include_once 'includes/head.php' ?>
