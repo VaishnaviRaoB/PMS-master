9e821e126deccdb19f3a9672af4cf3c813277f19
VaishnaviRaoB <vaishnaviraob61@gmail.com>
2024-05-31 19:55:53 +0530

    Changes

--- includes/nav.php
+++ includes/nav.php
@@ -10,9 +10,7 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active" id="home">
-        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-      </li>
+     
      
       <li class="nav-item" id="about">
         <a class="nav-link" href="about.php">About Us</a>
