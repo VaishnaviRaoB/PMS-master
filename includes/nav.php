461116acca26ddb8f78f37c2945e994cb48eb5e3
shreelaxmikamath <shreelaxmi.k111@gmail.com>
2024-05-31 20:10:54 +0530

    nav updated

--- includes/nav.php
+++ includes/nav.php
@@ -3,7 +3,7 @@
   session_destroy();
 ?>
 <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
-  <a class="navbar-brand" href="#"><img src="images/smv.png" style="width: 30px; height: 30px;"></a>
+  <a class="navbar-brand" href="#"><img src="images/smv.png" style="width: 60px; height: 50px;"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -13,7 +13,7 @@
      
      
       <li class="nav-item" id="about">
-        <a class="nav-link" href="about.php">About Us</a>
+        <a class="nav-link" href="about.php" style="font-size:large">About Us</a>
       </li>
      
     </ul>
