d8fe6b65a0a222b95bc3e15d5f2a0cf0549fcc81
VaishnaviRaoB <vaishnaviraob61@gmail.com>
2024-06-01 17:52:23 +0530

    Changes

--- index.php
+++ index.php
@@ -10,28 +10,27 @@
     <?php include_once 'includes/head.php' ?>
 </head>
 <body>
-
     <img src="images/homeback1.png" id="img1">
     <div style="position: absolute; margin-left:56%; margin-top:175px;">
-    <img src="images/task.svg" width="430px" style="z-index: 1;">
+        <img src="images/task.svg" width="430px" style="z-index: 1;">
     </div>
-    	<?php include_once 'includes/nav.php' ?>
+    <?php include_once 'includes/nav.php' ?>
     <div class="content" style="margin-top: 160px; margin-left: 10px;">
-    	<h1 style="margin-left: 30px; font-size: 64px;"><b> </b><br> PLACEMENT <br><b> MANAGEMENT </b><br> SYSTEM</h1> <br>
-        <!-- <img src="images/logo1.png" width="550px"> -->
+        <h1 style="margin-left: 30px; font-size: 64px;"><b> </b><br> PLACEMENT <br><b> MANAGEMENT </b><br> SYSTEM</h1> <br>
     </div>
-
-      <div id="contact" style="z-index: 1; position: absolute; margin-left:2%; margin-top: 30vh;">
-            <div class="form-row">
-             <div class="form-group" style="margin-left:270px; margin-top: 900px;"> 
+    <div id="contact" style="z-index: 1; position: absolute; margin-left:2%; margin-top: 30vh;">
+        <div class="form-row">
+            <div class="form-group" style="margin-left:270px; margin-top: 900px;"> 
+                <div>
                 <h1>Contact Us</h1>
                 <p class="lead">&nbsp;<i class="fas fa-mobile"></i>&nbsp; (+91) 1234567890</p>
                 <p class="lead">&nbsp;<i class="fas fa-at"></i>&nbsp; admin@gmail.com</p>
-              </div>
-              <div class="form-group" >
-              <img src="images/contact.svg" alt="..." style="width: 600px; margin-left: 30%; margin-top: -25%;">
-              </div>
+                </div>
             </div>
+            <div class="form-group" >
+                <img src="images/contact.svg" alt="..." style="width: 600px; margin-left: 30%; margin-top: -15%;">
+            </div>
+        </div>
     </div>
     <?php include_once 'includes/footer.php' ?>
 </body>
